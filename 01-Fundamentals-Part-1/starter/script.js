3d3a8fb4a15a79cb13a847410a55d126eafee959
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-10-07 00:27:27 +0600

    Data Types lesson

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,27 @@
+/*let js = "amazing";
+if (js === "amazing") alert("Javasscript is fun!");
+console.log(5 + 5);
+
+console.log("Jonas");
+
+let firstName = "Bob";
+
+console.log(firstName);
+
+let country = "Kazakhstan";
+let continent = "Eurasia";
+let population = 1900000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+// console.log(type(country));
+
+//comment
+Multiline comments 
+*/
+true;
+console.log(true);
+let JavascriptIsFun = true;
+console.log(JavascriptIsFun);
+console.log(typeof null);
